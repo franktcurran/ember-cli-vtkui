ff5be626bfa1c7041da0432589157110bc1bd4e6
Ryan Connolly <rynam0@gmail.com>
2016-03-08 08:59:11 -0500

    update tests to match new input component api

--- tests/integration/components/g-input-test.js
+++ tests/integration/components/g-input-test.js
@@ -1,11 +1,13 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('g-input', 'Integration | Component | g input', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  this.render(hbs`{{g-input value="my text"}}`);
-  assert.equal(this.$('input').val().trim(), 'my text');
+  this.set('model', Ember.Object.create({ name: 'Qunit' }));
+  this.render(hbs`{{g-input model=model path="name"}}`);
+  assert.equal(this.$('input').val().trim(), 'Qunit');
 });
