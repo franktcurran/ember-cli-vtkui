1913409618cf7c2aa04121d8a43b94ff175069fc
Ryan Connolly <rynam0@gmail.com>
2016-03-10 21:22:25 -0500

    update transitions

--- tests/dummy/app/transitions.js
+++ tests/dummy/app/transitions.js
@@ -1,7 +1,41 @@
 export default function() {
+  // this.transition(
+  //     this.hasClass('ember-view'),
+  //     this.use('toDown')
+  // );
   this.transition(
-      this.hasClass('ember-view'),
+      this.fromRoute('index'),
       this.use('toDown'),
       this.reverse('toUp')
+  );
+  this.transition(
+      this.fromRoute('panels'),
+      this.use('toLeft'),
+      this.reverse('toRight')
+  );
+  this.transition(
+      this.fromRoute('inputs'),
+      this.use('toLeft'),
+      this.reverse('toRight')
+  );
+  this.transition(
+      this.fromRoute('combos'),
+      this.use('toLeft'),
+      this.reverse('toRight')
+  );
+  this.transition(
+      this.fromRoute('buttons'),
+      this.use('toLeft'),
+      this.reverse('toRight')
+  );
+  this.transition(
+      this.fromRoute('grids'),
+      this.use('toLeft'),
+      this.reverse('toRight')
+  );
+  this.transition(
+      this.fromRoute('paginator'),
+      this.use('toLeft'),
+      this.reverse('toRight')
   );
 }
