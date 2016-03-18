2fcb5ca2881c882f1a39f4c11526661c2a099df4
Ryan Connolly <rynam0@gmail.com>
2016-03-18 11:56:35 -0400

    start documenting the addon

--- tests/dummy/app/transitions.js
+++ tests/dummy/app/transitions.js
@@ -1,41 +1,6 @@
 export default function() {
-  // this.transition(
-  //     this.hasClass('ember-view'),
-  //     this.use('toDown')
-  // );
   this.transition(
-      this.fromRoute('index'),
-      this.use('toDown'),
-      this.reverse('toUp')
-  );
-  this.transition(
-      this.fromRoute('panels'),
-      this.use('toLeft'),
-      this.reverse('toRight')
-  );
-  this.transition(
-      this.fromRoute('inputs'),
-      this.use('toLeft'),
-      this.reverse('toRight')
-  );
-  this.transition(
-      this.fromRoute('combos'),
-      this.use('toLeft'),
-      this.reverse('toRight')
-  );
-  this.transition(
-      this.fromRoute('buttons'),
-      this.use('toLeft'),
-      this.reverse('toRight')
-  );
-  this.transition(
-      this.fromRoute('grids'),
-      this.use('toLeft'),
-      this.reverse('toRight')
-  );
-  this.transition(
-      this.fromRoute('paginator'),
-      this.use('toLeft'),
-      this.reverse('toRight')
+      this.hasClass('ember-view'),
+      this.use('fade')
   );
 }
