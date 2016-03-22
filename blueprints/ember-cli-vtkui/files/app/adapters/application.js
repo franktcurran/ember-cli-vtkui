e7509869f49d9145c5ebae509e7bae7e9a90b0d4
Ryan Connolly <rynam0@gmail.com>
2016-03-22 16:44:04 -0400

    update application adapter.

--- blueprints/ember-cli-vtkui/files/app/adapters/application.js
+++ blueprints/ember-cli-vtkui/files/app/adapters/application.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: `${window.location.pathname.replace('/', '')}api`
+
+  namespace: function() {
+    let path = window.location.pathname;
+    let slash = path.lastIndexOf('/');
+    let hasSlash = slash !== -1;
+    return (hasSlash) ? `${path.substr(0, slash)}/api` : `${path}/api`;
+  }.property()
+
 });
