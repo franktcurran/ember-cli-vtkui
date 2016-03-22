7865426bcb9d62272f439b6a6b9660b355b8d2c6
Ryan Connolly <rynam0@gmail.com>
2016-03-22 07:29:01 -0400

    fix test

--- tests/integration/components/g-grid-heading-test.js
+++ tests/integration/components/g-grid-heading-test.js
@@ -8,17 +8,6 @@ moduleForComponent('g-grid-heading', 'Integration | Component | g grid heading',
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
-
   this.render(hbs`{{g-grid-heading}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#g-grid-heading}}
-      template block text
-    {{/g-grid-heading}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '△');
 });
